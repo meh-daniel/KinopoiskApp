336ada21a3c8b3037458adc96dd74e6352ccb8d4
Danil Galkin <mehanikdanil@gmail.com>
2022-10-18 12:27:15 +0700

    добавление компонента логики приложения

---
+++ app/src/main/java/meh/daniel/com/kinopoiskapp/di/MovieComponentModule.kt
@@ -0,0 +1,36 @@
+package meh.daniel.com.kinopoiskapp.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+import meh.daniel.com.kinopoiskapp.data.MovieRepositoryImpl
+import meh.daniel.com.kinopoiskapp.data.network.KinopoiskApi
+import meh.daniel.com.kinopoiskapp.domain.MovieRepository
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+class SerialComponentModule {
+
+    @Provides
+    @Singleton
+    fun provideKinopoiskApi(
+        @Named("retrofitKinopoiskBuilder") retrofit: Retrofit
+    ) : KinopoiskApi {
+        return retrofit.create(KinopoiskApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSerialRepository(
+        api: KinopoiskApi,
+    ) : MovieRepository {
+        return MovieRepositoryImpl(api)
+    }
+
+}
