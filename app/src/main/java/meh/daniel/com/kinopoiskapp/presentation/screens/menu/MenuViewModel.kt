22a8c765e51f8c6ff409ae55398e79c3e59f4a93
Danil Galkin <mehanikdanil@gmail.com>
2022-10-18 14:12:51 +0700

    добавление нижней навигации

--- app/src/main/java/meh/daniel/com/kinopoiskapp/presentation/screens/menu/MenuViewModel.kt
+++ app/src/main/java/meh/daniel/com/kinopoiskapp/presentation/screens/menu/MenuViewModel.kt
@@ -1,7 +1,11 @@
 package meh.daniel.com.kinopoiskapp.presentation.screens.menu
 
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import meh.daniel.com.kinopoiskapp.core.BaseViewModel
+import meh.daniel.com.kinopoiskapp.domain.MovieRepository
 
 @HiltViewModel
-class MenuViewModel: BaseViewModel()
+class MenuViewModel @Inject constructor(
+    private val repository: MovieRepository
+): BaseViewModel()
