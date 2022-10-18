a828d78699a365407959e8329cc3220fedfe635d
Danil Galkin <mehanikdanil@gmail.com>
2022-10-18 21:09:05 +0700

    1. sql запросов и экземпляр создания бд

---
+++ app/src/main/java/meh/daniel/com/kinopoiskapp/data/storage/MovieDao.kt
@@ -0,0 +1,42 @@
+package meh.daniel.com.kinopoiskapp.data.storage
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import meh.daniel.com.kinopoiskapp.data.storage.model.CountrySW
+import meh.daniel.com.kinopoiskapp.data.storage.model.GenreSW
+import meh.daniel.com.kinopoiskapp.data.storage.model.MovieSW
+
+@Dao
+interface MovieDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCountries(countries: List<CountrySW>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertGenres(genres: List<GenreSW>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovies(movies: List<MovieSW>)
+
+    @Query("SELECT * FROM country")
+    suspend  fun getAllCountries() : List<CountrySW>
+
+    @Query("SELECT * FROM genre")
+    suspend  fun getAllGenres() : List<GenreSW>
+
+    @Query("SELECT * FROM movie")
+    suspend  fun getAllMovies() : List<MovieSW>
+
+    @Query("DELETE FROM movie")
+    suspend fun deleteAllMovies()
+
+    @Transaction
+    suspend fun updateLastMovies(movies: List<MovieSW>) {
+        deleteAllMovies()
+        insertMovies(movies)
+    }
+
+}
